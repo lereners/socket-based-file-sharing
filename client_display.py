07e3921d0643228048a1cb7b8ae338ce7d792e64
kholbyjack <kholbert.2015@gmail.com>
2025-11-11 23:11:36 -0500

    Some GUI changes
    Still doesn't look very good, I'll have to learn more about the grid system. Placing objects on the window has been a pain.

--- client_display.py
+++ client_display.py
@@ -7,21 +7,23 @@ from tkinter import ttk
 # There are two pages: the log-in page and the main page where the user will interact with the server
 # https://www.geeksforgeeks.org/python/tkinter-application-to-switch-between-different-page-frames/
 # ^highly used as a reference for the base structure
+# Maybe no loop? Just make the connection and logout/other commands will disconnect!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 class ServerApp(tk.Tk):
     # initializing app
     def __init__(self, *args, **kwargs):
         # initialize class Tk
         tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Socket Server -- Client-Side App")
 
         window_frame = tk.Frame(self)
         window_frame.pack(side="top", fill="both", expand=True)
-
+        
         window_frame.grid_rowconfigure(0, weight=1)
         window_frame.grid_columnconfigure(0, weight=1)
 
         # window size
-        self.geometry("725x750")
+        self.geometry("800x400")
 
         # an array for the two frames
         self.frames = {}
@@ -39,6 +41,8 @@ class ServerApp(tk.Tk):
     def display_frame(self, frame):
         current_frame = self.frames[frame]
         current_frame.tkraise()
+    
+    # while loop probably needs to go in here, we cannot keep connecting and disconnecting
 
 
 class SignIn(tk.Frame):
@@ -49,17 +53,21 @@ class SignIn(tk.Frame):
         title.grid(row=0, column=4, padx=10, pady=10)
         
         # entry boxes (Entry type)
-        # username = ""
-        # username_entry = ttk.Entry(textvariable=username)
-        # username_entry.grid(row=1, column=2, padx=10, pady=10)
+        username = tk.StringVar()
+        password = tk.StringVar()
+        username_entry = ttk.Entry(self, textvariable=username)
+        username_entry.grid(row=15, column=10)
+        password_entry = ttk.Entry(self, textvariable=password)
+        password_entry.grid(row=17, column=10)
 
         # creating sign in button, which takes you to the client page if valid credentials are entered
         button_signin = ttk.Button(self, text="Sign In", 
                         command = lambda: controller.display_frame(ClientPage))
-        button_signin.grid(row=3, column=2, padx=10,pady=10)
+        button_signin.grid(row=19, column=10, padx=10,pady=10)
 
+    # -----button commands-----
     # method for signing in, which will use server side commands
-    # def login_clicked(self):
+    # def signin_clicked(self):
     #     print("logged in")
 
 
@@ -68,12 +76,43 @@ class ClientPage(tk.Frame):
     def __init__(self, parent, controller):
         # 
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Server Files", font=("Verdana", 35))
+        # window title
+
+        # label
+        label = ttk.Label(self, text="Server Files", font=("Verdana", 20))
         label.grid(row=0, column=4, padx=10, pady=10)
 
         # file display (need a scroll feature and maybe a separate frame displayed here)
+        file_box = tk.Listbox(self, height=15, width=80)
+        file_box.grid(row=1, column=2, rowspan=4, columnspan=5)
+        
+        # -----buttons------
+        style = ttk.Style()
+        style.configure('W.TButton', foreground='red')
+        logout_button = ttk.Button(self, text="Log Out", 
+                        command = lambda: controller.display_frame(SignIn))
+        logout_button.grid(row=15,column=10, padx=10, pady=10)
+        dir_button = ttk.Button(self, text="Dir", 
+                        command = None)
+        dir_button.grid(row=14, column=1, padx=10, pady=10)
+        upload_button = ttk.Button(self, text="Upload", 
+                        command = None)
+        upload_button.grid(row=15, column=1, padx=10, pady=10)
+        download_button = ttk.Button(self, text="Download", 
+                        command = None)
+        download_button.grid(row=15, column=2, padx=10, pady=10)
+        new_button = ttk.Button(self, text="New Directory", 
+                        command=None)
+        new_button.grid(row=15, column=3, padx=10, pady=10)
+        delete_button = ttk.Button(self, text="Delete", style='W.TButton', 
+                        command=None)
+        
+        delete_button.grid(row=15, column=4, padx=10, pady=10)
 
-        # buttons
+    # -----button commands-----
+    def fetch_files(self):
+        # for loop for inserting files from server
+        print("hiiiii")
 
     # definitions for what the buttons actually do????
     # maybe add a log out button
@@ -81,10 +120,6 @@ class ClientPage(tk.Frame):
     # def display_files(self):
     #     # populate listbox with the file names
     #     print("diplayed")
-
-
-
-
 
 # running the file
 def main():
