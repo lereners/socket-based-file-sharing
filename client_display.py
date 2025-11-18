15dcbc84b72550111d0b872ae9a3025bddcae2be
kholbyjack <kholbert.2015@gmail.com>
2025-11-18 00:46:43 -0500

    Added dir functionality and some QOL
    - Dir now displays the files in the listbox
    - There is an option to select a file from the user's device (NOTE: currently does not add to what's in the text box)
    - Added a button to clear the entry box

--- client_display.py
+++ client_display.py
@@ -1,8 +1,10 @@
 from client_file_commands import *
 from client import *
+from client import serv_response
 import socket
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 
 
 # using classes to organize different frames easily
@@ -13,32 +15,32 @@ from tkinter import ttk
 
 LOGIN = False
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+serv_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-def connect(client):
-    client.connect(ADDR)
-    data = client.recv(SIZE).decode(FORMAT)
+def connect(serv_client):
+    serv_client.connect(ADDR)
+    data = serv_client.recv(SIZE).decode(FORMAT)
     LOGIN = True
     handle_response(data)
 
-def disconnect(client):
+def disconnect(serv_client):
 
     print("Disconnected from the server.")
-    client.close() ## close the connection
+    serv_client.close() ## close the connection
     
 def complete_command(data):
     split = data.split(" ")                     # delimiting w/ space is not ideal, esp if user provides path w/ spaces -> file won't be found
     cmd = split[0].upper()
     arg1 = split[1] if len(split) > 1 else None # first argument, if provided
     arg2 = split[2] if len(split) > 2 else None # second argument, if provided
-    process_command(cmd, client, split, arg1, arg2)
+    process_command(cmd, serv_client, split, arg1, arg2)
 
 class ServerApp(tk.Tk):
     # initializing app
     def __init__(self, *args, **kwargs):
         # initialize class Tk
         tk.Tk.__init__(self, *args, **kwargs)
-        self.title("Socket Server -- Client-Side App")
+        self.title("Socket Server -- serv_client-Side App")
 
         window_frame = tk.Frame(self)
         window_frame.pack(side="top", fill="both", expand=True)
@@ -47,12 +49,12 @@ class ServerApp(tk.Tk):
         window_frame.grid_columnconfigure(0, weight=1)
 
         # window size
-        self.geometry("800x400")
+        self.geometry("800x450")
 
         # an array for the two frames
         self.frames = {}
 
-        for frame in (SignIn, ClientPage):
+        for frame in (SignIn, serv_clientPage):
             # adding the different frames into the array
             current_frame = frame(window_frame, self)
             self.frames[frame] = current_frame
@@ -84,14 +86,14 @@ class SignIn(tk.Frame):
         password_entry = ttk.Entry(self, textvariable=password)
         password_entry.grid(row=17, column=10)
 
-        # creating sign in button, which takes you to the client page if valid credentials are entered
+        # creating sign in button, which takes you to the serv_client page if valid credentials are entered
         button_signin = ttk.Button(self, text="Sign In", 
-                        command = lambda: [controller.display_frame(ClientPage), connect(client)])
+                        command = lambda: [controller.display_frame(serv_clientPage), connect(serv_client)])
         
         # make new function for authentication, also new button for register below sign in
         # display message saying that sign in and register uses same input boxes
         button_signin.grid(row=19, column=10, padx=10,pady=10)
-        # will be [controller.display_frame(ClientPage), process_command()]
+        # will be [controller.display_frame(serv_clientPage), process_command()]
         # with the password and username
     # -----button commands-----
     # method for signing in, which will use server side commands
@@ -99,8 +101,8 @@ class SignIn(tk.Frame):
     #     print("logged in")
 
 
-class ClientPage(tk.Frame):
-    # initializing client page
+class serv_clientPage(tk.Frame):
+    # initializing serv_client page
     def __init__(self, parent, controller):
         # 
         tk.Frame.__init__(self, parent)
@@ -124,14 +126,20 @@ class ClientPage(tk.Frame):
         # file_box.configure(yscrollcommand=scrollbar.set)
 
         file_name = tk.StringVar()
-        file_entry = tk.Entry(self, textvariable=file_name)
+        file_entry = tk.Entry(self, textvariable=file_name, width = 30)
         file_entry.grid(row=13, column=2)
+
+        device_button = ttk.Button(self, text="From Device", command=lambda : self.get_path(file_entry, file_name))
+        device_button.grid(row=13, column=3)
+
+        clearbox_button = ttk.Button(self, text="Clear Entry", command=lambda : self.clear_entrybox(file_entry, file_name))
+        clearbox_button.grid(row=13, column=4)
 
         # -----buttons------
         style = ttk.Style()
         style.configure('W.TButton', foreground='red')
         logout_button = ttk.Button(self, text="Log Out", 
-                        command = lambda: [controller.display_frame(SignIn), complete_command("LOGOUT"), disconnect(client)])
+                        command = lambda: [controller.display_frame(SignIn), complete_command("LOGOUT"), disconnect(serv_client)])
         logout_button.grid(row=15,column=10, padx=10, pady=10)
         dir_button = ttk.Button(self, text="Dir", 
                         command = lambda : self.dir_clicked(file_name, file_box))
@@ -143,7 +151,7 @@ class ClientPage(tk.Frame):
                         command = lambda : complete_command("DOWNLOAD " + file_name.get()))
         download_button.grid(row=15, column=2, padx=10, pady=10)
         new_button = ttk.Button(self, text="Create Subfolder", 
-                        command= lambda : complete_command("SUBFOLDER " + file_name.get()))
+                        command= lambda : complete_command("SUBFOLDER CREATE " + file_name.get()))
         new_button.grid(row=15, column=3, padx=10, pady=10)
         delete_button = ttk.Button(self, text="Delete", style='W.TButton', 
                         command=lambda : complete_command("DELETE " + file_name.get()))
@@ -156,22 +164,37 @@ class ClientPage(tk.Frame):
         # check for if there is a filename listed
         data = "DIR " + file_name.get()
         print(data)
-        list_files = complete_command(data)
-        print(list_files)
-    
-        # for name in list_files:
-        #     file_box.insert('end', name)
+        complete_command(data)
+        list_files = return_servresponse().split()
+        print(list_files[0])
+
+        file_box.delete(0, tk.END)
+        for name in list_files:
+            file_box.insert(tk.END, name)
 
         file_box.grid(row=1, column=2, rowspan=4, columnspan=5)
+
+    def get_path(self, file_entry, file_name):
+        # for selecting from device
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            file_name = file_path
+            print(f"{file_name} was selected.")
+            file_entry.insert(0, file_path)
+
+    def clear_entrybox(self, file_entry, file_name):
+        # clears the entry box
+        file_name = ""
+        file_entry.delete(0, tk.END)
         
     # def upload_clicked(self):
     #     print("AAAA")
 
-    def download_clicked(self, file_name):
-        if(file_name == None):
-            print("No File Selected. Enter the filename into the textbox or select a file from the list.")
-            return
-        name = file_name.get()
+    # def download_clicked(self, file_name):
+    #     if(file_name == None):
+    #         print("No File Selected. Enter the filename into the textbox or select a file from the list.")
+    #         return
+    #     name = file_name.get()
 
 
 # running the file
